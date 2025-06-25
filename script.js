ebd11388f62d25b268fccae2ee71e0fed322126f
Jeferson1505 <jefersonriosmejia38@gmail.com>
2025-06-25 12:29:20 -0500

    hoja de vida ok

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function downloadPDF() {
-    const element=document.querySelector(".container-lg");
-    const opt={
-        margin: 0,
-        filename: 'Hoja_de_vida_jeferson_rios.pdf',
-        image: {type: 'jpeg', quality:1},
-        html2canvas:{scale:2,scrolly:0},
-        jsPDF:{unit:'mm',format:'letter',orientation:'portrait'},
-        /* pagebreak:{mode:['avoid-all','css','legacy']} */
+    const element = document.querySelector('.container-lg');
+
+    const opt = {
+        margin: [0, 5, 0 , 5],  // [arriba, izquierda, abajo, derecha] en mm
+        filename: 'Hoja_de_Vida_jeferson_rios.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: {
+            scale: 2,
+            useCORS: true,
+            allowTaint: false,
+            scrollY: 0
+        },
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        }
     };
+
     html2pdf().set(opt).from(element).save();
-    
 }
